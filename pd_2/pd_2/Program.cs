118c53644ac40f6e6bf36d66c2ed524f539a5749
burkaans <raimonds.buholts@gmail.com>
2018-05-18 18:57:47 +0300

    pd_2
    3. uzdevums done

--- pd_2/pd_2/Program.cs
+++ pd_2/pd_2/Program.cs
@@ -12,7 +12,34 @@ namespace pd_2
         {
             Console.WriteLine(Aprekins1(7, 3));
             Console.WriteLine(Aprekins2(10, 3));
-            Console.ReadLine();
+
+            int izvele = 1;
+
+            while (izvele != 0)
+            {
+                Console.WriteLine("1- Izvadit saskaitisanas funkciju");
+                Console.WriteLine("2- izvadit atnemsanas funkciju");
+                Console.WriteLine("0- Iziet");
+
+                string input = Console.ReadLine();
+                izvele = Convert.ToInt16(input);
+
+                switch (izvele)
+                {
+                    case 1:
+                        Console.WriteLine(Aprekins1(3, 7));
+                        break;
+                    case 2:
+                        Console.WriteLine(Aprekins2(10, 7));
+                        break;
+                    case 0:
+                        izvele = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Ievade nav pareiza");
+                        break;
+                }
+            }
         }
         static double Aprekins1(double a, double b)
         {
