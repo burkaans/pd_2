067611a67cba52eb43c5b2e52d6f538224543bb2
burkaans <raimonds.buholts@gmail.com>
2018-05-18 18:32:28 +0300

    pd_2
    1.commit

---
+++ pd_2/pd_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pd_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Master!");
+            Console.ReadLine();
+        }
+    }
+}
